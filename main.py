3c8117723f381e6b437f863071ff0cc95f8807c7
goonmandu <61984863+goonmandu@users.noreply.github.com>
2022-02-17 00:57:29 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 import os
+import sys
 import random
 from termcolor import cprint, colored
 
+
+REL_PATH = os.path.dirname(__file__)
+FILES = [f for f in os.listdir(REL_PATH) if os.path.isfile(os.path.join(REL_PATH, f)) and f not in ["LICENSE", "README.md"] and not f.endswith(".py")]
 APP_NAME = "PYRDLE"
-VERSION = "1.1.3"
-WORD_FILE = "wordle"
-# WORD_FILE = "words"
+VERSION = "1.2.4"
+WORD_FILE = None
 
-if os.name in ["nt", "dos"]:
-    os.system("cls")
-else:
-    os.system("clear")
 
+def clear_console():
+    if os.name in ["nt", "dos"]:
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def ask_in_console():
+    global WORD_FILE
+    clear_console()
+    print(f"Wordsets: {', '.join(FILES)}")
+    WORD_FILE = input("Pick a wordset: ")
+    if WORD_FILE not in FILES:
+        ask_in_console()
+
+
+def ask_wordset():
+    global WORD_FILE
+    clear_console()
+    if len(sys.argv) < 2:
+        ask_in_console()
+    elif sys.argv[1] in FILES:
+        WORD_FILE = sys.argv[1]
+    else:
+        ask_in_console()
+
+
+ask_wordset()
 with open(WORD_FILE) as f:
     words = [word.strip("\n") for word in f.readlines()]
 
@@ -32,6 +59,8 @@ def take_guess():
     guess = input().lower()
     answer_list = [ltr for ltr in answer]
     guess_list = [ltr for ltr in guess]
+    if guess == "?????":
+        return True
     if len(guess) == 5 and guess in words:
         for index in range(5):
             if answer[index] == guess[index]:
@@ -54,13 +83,14 @@ def take_guess():
         cprint("INVAL", "grey", "on_red")
 
 
+clear_console()
+
 cprint(f"{APP_NAME}", "grey", "on_green", end="")
 cprint(f"v{VERSION}", "grey", "on_yellow", end="")
 cprint(f"{WORD_FILE}\n", "grey", "on_white")
 
 while guesses:
-    take_guess()
-    if answer_list == [0, 0, 0, 0, 0]:
+    if take_guess() or answer_list == [0, 0, 0, 0, 0]:
         break
 print()
 cprint("WORD:", "grey", "on_magenta")
