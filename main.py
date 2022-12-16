a4aee3ee1b33c5b2900397ac8503ffc5a93054fa
goonmandu <hwang.mingi7@gmail.com>
2022-12-16 22:04:03 +0100

    Rewrite Pyrdle and replace version 1

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import time
 import random
 from termcolor import cprint, colored
 
-
 REL_PATH = os.path.dirname(__file__)
 WORDSET_PATH = f"{REL_PATH}/wordsets"
 FILES = [f for f in os.listdir(WORDSET_PATH) if os.path.isfile(os.path.join(WORDSET_PATH, f))]
 APP_NAME = "PYRDLE"
-VERSION = "1.4.3"
+VERSION = "2.0.1"
 word_file = None
+tries = 6
 
 
 def clear_console():
@@ -40,8 +40,31 @@ def ask_wordset():
         ask_in_console()
 
 
-# os.system('mode con: cols=22 lines=14')
-# TODO: Fix automatic console window resizing
+def print_app_header():
+    cprint(f"{APP_NAME}", "grey", "on_green", end="")
+    cprint(f"v{VERSION}", "grey", "on_yellow", end="")
+    cprint(f"{word_file}\n", "grey", "on_white")
+
+
+def evaluate_guess(guess, against) -> list[int]:
+    guess = guess.lower()
+    against = against.lower()
+    letters_left_in_answer = [ltr for ltr in against]
+    evaluation = [0] * 5
+    # Check for green matches first so that yellows do not eat up green letters
+    # Greens are 2, Yellows are 1, and Grays are 0
+    for idx, ltr in enumerate(guess):
+        if against[idx] == guess[idx] and ltr in letters_left_in_answer:
+            evaluation[idx] = 2
+            letters_left_in_answer.remove(ltr)
+    for idx, ltr in enumerate(guess):
+        # evaluation[idx] != 2 prevents existing greens from being overwritten with a yellow
+        # when there are duplicate letters in the answer
+        if ltr in against and ltr in letters_left_in_answer and evaluation[idx] != 2:
+            evaluation[idx] = 1
+            letters_left_in_answer.remove(ltr)
+    return evaluation
+
 
 ask_wordset()
 with open(f"{WORDSET_PATH}/{word_file}") as f:
@@ -49,61 +72,57 @@ with open(f"{WORDSET_PATH}/{word_file}") as f:
 
 answer = random.choice(words)
 
-# DEBUG, COMMENT OUT
-# answer = input()
+print_app_header()
 
-correct_letters = 0
-guesses = 6
-char_list = [" "] * 5
-answer_list = []
-guess_list = []
+while True:
+    # If the user has run out of tries,
+    if tries == 0:
+        # Show answer and exit
+        cprint(answer.upper(), "grey", "on_magenta")
+        break
+    char_list = [] * 5
+    user_guess = input()
 
+    # If the user requests the answer,
+    if user_guess == "?????":
+        # Show answer and exit
+        cprint(answer.upper(), "grey", "on_magenta")
+        break
 
-def take_guess():
-    global answer_list, guess_list, guesses
-    guess = input().lower()
-    answer_list = [ltr for ltr in answer]
-    guess_list = [ltr for ltr in guess]
-    if guess == "?????":
-        return True
-    # DEBUG if len(guess) == 5:
-    if len(guess) == 5 and guess in words:
-        for index in range(5):
-            if answer[index] == guess[index]:
-                char_list[index] = colored(guess[index].upper(), "grey", "on_green")
-                answer_list[index] = 0
-                guess_list[index] = None
-        for index, char in enumerate(guess_list):
-            if char in answer_list:
-                char_list[index] = colored(guess[index].upper(), "grey", "on_yellow")
-                answer_list[answer_list.index(char)] = None
-                guess_list[index] = None
-        for index, char in enumerate(guess_list):
-            if char:
-                char_list[index] = colored(guess[index].upper(), "grey", "on_white")
-        cprint(f"\033[1A{''.join(char_list)}")
-        guesses -= 1
-    elif len(guess) != 5:
+    # If the guess length is not 5,
+    if len(user_guess) != 5:
+        # Print warning and let user guess again
         cprint("\033[1AFIVE!", "grey", "on_red", end="")
-        print(" " * (len(guess) - 5) if len(guess) > 5 else "", end="\r")
+        print(" " * (len(user_guess) - 5) if len(user_guess) > 5 else "", end="\r")
         time.sleep(1)
         print("     ", end="\r")
-    else:
+        continue
+
+    # If the guessed word is not in the chosen wordset:
+    if user_guess not in words:
+        # Print warning and let user guess again
         cprint("\033[1AINVAL", "grey", "on_red", end="\r")
         time.sleep(1)
         print("     ", end="\r")
+        continue
 
+    # If the guess is a valid 5 letter word, evaluate
+    result = evaluate_guess(user_guess, answer)
+    tries -= 1
 
-clear_console()
+    # Format result string according to the list returned by evaluate_guess()
+    for idx, value in enumerate(result):
+        if value == 2:
+            char_list.append(colored(user_guess[idx].upper(), "grey", "on_green"))
+        elif value == 1:
+            char_list.append(colored(user_guess[idx].upper(), "grey", "on_yellow"))
+        else:
+            char_list.append(colored(user_guess[idx].upper(), "grey", "on_white"))
 
-cprint(f"{APP_NAME}", "grey", "on_green", end="")
-cprint(f"v{VERSION}", "grey", "on_yellow", end="")
-cprint(f"{word_file}\n", "grey", "on_white")
+    # Print result string
+    cprint(f"\033[1A{''.join(char_list)}")
 
-while guesses:
-    if take_guess() or answer_list == [0, 0, 0, 0, 0]:
+    # If the user guessed correctly,
+    if result == [2] * 5:
+        # Exit
         break
-print()
-if answer_list != [0, 0, 0, 0, 0]:
-    cprint("WORD:", "grey", "on_magenta")
-    cprint(answer.upper(), "grey", "on_cyan")
